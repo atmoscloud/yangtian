7cf6222dd7a4c1616738bb83a0bd48176d7eb1c5
atmoscloud <14917937+atmoscloud@users.noreply.github.com>
2025-10-06 18:44:11 +0000

    Deploying to gh-pages from @ atmoscloud/yangtian@eda1078e8fc092acc11981d06a8299a482f98248 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -3,8 +3,8 @@ const ninja = document.querySelector('ninja-keys');
 
 // add the home and posts menu items
 ninja.data = [{
-    id: "nav-",
-    title: "",
+    id: "nav-yang-tian",
+    title: "Yang Tian",
     section: "Navigation",
     handler: () => {
       window.location.href = "/yangtian/";
