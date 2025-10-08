efa8f2bbb052b97d649f4f271db6a43119d9e1e6
atmoscloud <14917937+atmoscloud@users.noreply.github.com>
2025-10-08 22:46:21 +0000

    Deploying to gh-pages from @ atmoscloud/yangtian@3c18cec562f0b0a82f43fd067fe19fff2517af1a 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -416,9 +416,9 @@ ninja.data = [{
             },},{id: "news-a-simple-inline-announcement-with-markdown-emoji-sparkles-smile",
           title: 'A simple inline announcement with Markdown emoji! :sparkles: :smile:',
           description: "",
-          section: "News",},{id: "projects-project-1",
-          title: 'project 1',
-          description: "with background image",
+          section: "News",},{id: "projects-high-resolution-les-crm",
+          title: 'High resolution LES/CRM',
+          description: "Process-level understanding of cloud-environment interaction",
           section: "Projects",handler: () => {
               window.location.href = "/yangtian/projects/1_project/";
             },},{id: "projects-project-2",
