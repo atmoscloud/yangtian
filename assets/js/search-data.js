d2b7137f64096895a272b29c9e2cc9bb83edb6f5
atmoscloud <14917937+atmoscloud@users.noreply.github.com>
2025-10-09 02:51:28 +0000

    Deploying to gh-pages from @ atmoscloud/yangtian@875b1083aa1c7ed6e09db817836f5f9b48821876 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -24,7 +24,7 @@ ninja.data = [{
             window.location.href = "/yangtian/publications/";
           },
         },{id: "nav-teaching",
-          title: "teaching",
+          title: "Teaching",
           description: "",
           section: "Navigation",
           handler: () => {
