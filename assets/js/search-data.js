da9a8084bede6a9457b3645062cb1a25fed64a13
atmoscloud <14917937+atmoscloud@users.noreply.github.com>
2025-10-09 05:00:32 +0000

    Deploying to gh-pages from @ atmoscloud/yangtian@88fc391cf3f1d21e33b5110a347e4793c529d2c9 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -11,7 +11,7 @@ ninja.data = [{
     },
   },{id: "nav-research",
           title: "Research",
-          description: "A growing collection of my projects.",
+          description: "A growing collection of my projects",
           section: "Navigation",
           handler: () => {
             window.location.href = "/yangtian/projects/";
